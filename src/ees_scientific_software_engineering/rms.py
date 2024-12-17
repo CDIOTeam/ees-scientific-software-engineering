0b20538b737b19485b251e66e30aa4db899784fe
TuPhan <thanhtuphanhuu@gmail.com>
2024-12-17 11:28:44 +0100

    Add error handling for one dimension

--- src/ees_scientific_software_engineering/rms.py
+++ src/ees_scientific_software_engineering/rms.py
@@ -1,3 +1,6 @@
+"""
+    This module to calculate the RMS of an array
+"""
 import numpy as np
 
 
@@ -9,5 +12,12 @@ def rms(input_array: np.ndarray) -> float:
 
     Returns:
         root mean square number
+
+    Raises:
+        ValueError: If input_array is not one-dimensional
     """
+
+    if input_array.ndim != 1:
+        raise ValueError("input_array must be one-dimensional")
+
     return np.sqrt(np.mean(input_array**2))
--- tests/test_rms.py
+++ tests/test_rms.py
@@ -1,3 +1,6 @@
+"""
+    This module to calculate the RMS of an array
+"""
 import numpy as np
 import pytest
 
@@ -5,7 +8,18 @@ from ees_scientific_software_engineering.rms import rms
 
 
 def test_rms():
+    """
+        Testing root mean square
+    """
     input_array = np.array([0.0, 1.0, 2.0, 3.0, 4.0])
     result = np.sqrt((1.0**2 + 2.0**2 + 3.0**2 + 4.0**2) / 5)
     isinstance(rms(input_array), float)
     assert np.isclose(rms(input_array), result)
+
+def test_rms_invalid_dimension():
+    """
+        Testing dimension of numpy array
+    """
+    input_array = np.array([[1.0, 2.0], [3.0, 4.0]])  # 2D array
+    with pytest.raises(ValueError, match="input_array must be one-dimensional"):
+        rms(input_array)
