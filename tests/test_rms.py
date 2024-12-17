3181b5ce655d8dde257812ee17c8e5c7f3294097
bartimore <bartvdholst@hotmail.nl>
2024-12-17 10:54:20 +0100

    added docstring to test_msr

--- tests/test_rms.py
+++ tests/test_rms.py
@@ -5,6 +5,7 @@ from ees_scientific_software_engineering.rms import rms
 
 
 def test_rms():
+    """Check the output for the rms function for a simple case"""
     input_array = np.array([0.0, 1.0, 2.0, 3.0, 4.0])
     result = np.sqrt((1.0**2 + 2.0**2 + 3.0**2 + 4.0**2) / 5)
     isinstance(rms(input_array), float)
