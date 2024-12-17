27bdb2bd8d599d64e5450e433a89f046491552ca
TuPhan <thanhtuphanhuu@gmail.com>
2024-12-17 16:15:20 +0100

    Add new LU solver

---
+++ src/ees_scientific_software_engineering/lu_algebra.py
@@ -0,0 +1,25 @@
+"""
+    Library for LU solver
+"""
+import numpy as np
+
+# from scipy.linalg import lu_factor, lu_solve
+
+
+class LUSolver:
+    """
+    LU solver
+    """
+
+    def __init__(self, input_matrix: np.ndarray):
+        """
+        Constructor of the class. It takes the input matrix and decompose it into LU factorization.
+        Store the factorization and permutation into class members.
+        """
+        pass
+
+    def solve(self, b: np.ndarray) -> np.ndarray:
+        """
+        Solve the linear equation with the input matrix and the given vector b.
+        """
+        pass
